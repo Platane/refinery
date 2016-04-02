62853f38182f1c33d94dcf9a531b2748bf282558
Platane <me@platane.me>
2016-04-02 19:06:51 +0200

    leaf when no change on frag

--- test/dispatch-leaf.spec.js
+++ test/dispatch-leaf.spec.js
@@ -4,6 +4,26 @@ import {create}         from '../src'
 
 describe('leaf', () => {
 
+    describe('params', () => {
+
+        it('one frag with one leaf registred', () =>{
+
+            const A = ( action, previousValue ) =>
+                45
+
+            A.actions = [ 'z' ]
+
+            const x = create( {A} )
+            const fn = valueA =>
+                expect( valueA ).toBe( 45 )
+
+
+            x.register( A, fn )
+
+            x.dispatch({type:'z'})
+        })
+    })
+
     describe('register', () => {
 
         it('one frag with one leaf registred', () =>{
@@ -16,16 +36,35 @@ describe('leaf', () => {
             A.actions = [ 'z' ]
 
             const x = create( {A} )
-            const fn = valueA => {
-                expect( valueA ).toBe( 45 )
+            const fn = valueA =>
                 called = true
-            }
+
 
             x.register( A, fn )
 
             x.dispatch({type:'z'})
 
             expect( called ).toExist()
+        })
+
+        it('one frag with one leaf registred, fragment does not change', () =>{
+
+            let called = false
+
+            const A = ( action, previousValue ) =>
+                previousValue
+
+            A.actions = [ 'z' ]
+
+            const x = create( {A} )
+            const fn = valueA =>
+                called = true
+
+            x.register( A, fn )
+
+            x.dispatch({type:'z'})
+
+            expect( !called ).toExist()
         })
     })
 
@@ -41,10 +80,8 @@ describe('leaf', () => {
             A.actions = [ 'z' ]
 
             const x = create( {A} )
-            const fn = valueA => {
-                expect( valueA ).toBe( 45 )
+            const fn = valueA =>
                 called = true
-            }
 
             x.register( A, fn )
             x.unregister( fn )
