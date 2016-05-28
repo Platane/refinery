d2812258a957060eca05369285cc80b275933b0a
Platane <me@platane.me>
2016-05-28 17:49:23 +0200

    :tropical_fish:

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -26,7 +26,7 @@ module.exports = {
     },
 
     output: {
-        path: path.join(__dirname, 'lib'),
+        path: path.join(__dirname, 'dist'),
         filename: `[name]${ minify ? '.min' : '' }.js`,
     },
 
